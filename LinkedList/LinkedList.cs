273a0440aeec3775cf04b908b9510e6db351f5de
S Soundarya <soundarya.s@tvsnext.com>
2023-07-29 22:31:17 +0530

    [Soundarya]Add:Initial commit

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedListProblem
+{
+    public class LinkedList
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinkedListProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List operations");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+        }
+    }
+}
